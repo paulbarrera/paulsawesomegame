4076a150e5f2f37f4383a4e4092c6faf3c4f135d
Paul Barrera <paul@paulbarrera.com>
2020-10-19 18:53:30 -0400

    updated player.py

--- player.py
+++ player.py
@@ -8,13 +8,6 @@ class Player:
         self.last = last
         self.score = 0
 
-    def update_score(self):
-        self.clear()
-        self.write("Score: {}".format(self.score), font=("Arial", 14, "normal"))
-
-    def change_score(self, points):
-        self.score += points
-        self.update_score()
 
 
 #Player decides whether to be a goalkeeper or a shooter
@@ -24,5 +17,13 @@ class Player:
 #Player decides where they want to shoot 
 
 #Score is updated after every attempt
+
+    def update_score(self):
+        self.clear()
+        self.write("Score: {}".format(self.score), font=("Arial", 14, "normal"))
+
+    def change_score(self, points):
+        self.score += points
+        self.update_score()
 
 #After 5 attempts the game ends letting player know if they won or failed. 
